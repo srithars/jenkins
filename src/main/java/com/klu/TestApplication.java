7e916072ffa2cf844cf257b06302338ff5fc51fb
srithars <64460687+srithars@users.noreply.github.com>
2025-04-24 07:16:02 +0530

    Update TestApplication.java

--- src/main/java/com/klu/TestApplication.java
+++ src/main/java/com/klu/TestApplication.java
@@ -2,12 +2,18 @@ package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestApplication {
+public class TestApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
-
 }
